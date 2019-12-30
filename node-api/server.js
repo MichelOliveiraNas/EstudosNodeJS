15412b581a097d5a55b0304a47826b5de3b5ab2e
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2019-12-30 20:41:44 -0300

    Criação do arquivo de rotas e da pasta de Controller

--- node-api/server.js
+++ node-api/server.js
@@ -11,20 +11,7 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-
-// O GET É ACIONADO QUANDO O USUÁRIO ACESSAR A ROTA PASSADA NO PRIMEIRO PARÂMETRO.
-// O REQ SIMBOLIZA A REQUISIÇÃO QUE ESTÁ SENDO FEITA PARA O SERVIDOR. ELE CONTERÁ TODOS OS DETALHES E INFORMAÇÕES DESSA REQUISIÇÃO COMO PARÂMETROS, CORPO DA REQUISIÇÃO, USUÁRIO QUE FEZ A REQUISIÇÃO, ETC.
-// O RES SIMBOLIZA A RESPOSTA QUE SERÁ DADA PARA A REQUISIÇÃO.
-
-const Product  = mongoose.model('Product');
-
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'ReactJS',
-        description: 'Make web sites with React',
-        url: 'http://github.com/facebook/react'
-    });
-    return res.send('Hello World');
-});
+//ROTAS
+app.use('/api', require('./src/routes'));
 
 app.listen(3001); //DEFININDO A PORTA COMO 3001 DO NAVEGADOR.
