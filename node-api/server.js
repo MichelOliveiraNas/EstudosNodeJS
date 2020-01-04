268b7c7b9c21157061622e31594934a7979fa717
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-01-04 17:15:57 -0300

    Adicionando CORS

--- node-api/server.js
+++ node-api/server.js
@@ -1,15 +1,18 @@
 const express = require('express'); //IMPORTANDO O EXPRESS.
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 const app = express(); // DEFININDO A CONSTANTE 'APP' COMO UMA FUNÇÃO EXPRESS.
 app.use(express.json()); // PERMITIR O ENVIO DE DADOS PARA A APLICAÇÃO NO FORMATO DE JSON.
+app.use(cors());
 
 //INICIANDO O DB
 mongoose.connect(
     'mongodb://192.168.99.100:27017/nodeapi',
     { useNewUrlParser: true },
 );
+
 requireDir('./src/models');
 
 //ROTAS
