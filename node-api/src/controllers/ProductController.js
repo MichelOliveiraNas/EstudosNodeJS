3dfd6af40a9ad8954335f49779e563dfb02a4f5e
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-01-04 16:48:17 -0300

    Criação do CRUD

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -9,9 +9,29 @@ module.exports = {
         return res.json(products);
     },
 
-    async store(req, res) {
+    async show(req, res) {
+        const product = await Product.findById(req.params.id); //RECUPERANDO O ID QUE FOI PASSADO NOS PARÂMETROS DA ROTA NO ARQUIVO "ROUTES.JS".
+
+        return res.json(product);
+    },
+
+    async create(req, res) {
         const product = await Product.create(req.body);
 
         return res.json(product);
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        // RETORNANDO O PRODUTO COM ID PASSADO NO PARÂMETRO E ATUALIZANDO COM O CONTEUDO QUE FOR PASSADO NO REQ.BODY.
+        // "{ new: true }" = CASO ESSE PARÂMETRO NÃO SEJA PASSADO, O MONGOOSE RETORNA OS DADOS ANTIGOS ANTES DA ATUALIZAÇÃO ACONTECER.
+
+        return res.json(product);
+    },
+
+    async delete(req, res) {
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
 };
--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -3,7 +3,19 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
+//TRAZENDO TODOS OS DADOS
 routes.get('/products', ProductController.index);
-routes.post('/products', ProductController.store);
+
+//BUSCANDO UM DADO PELO ID
+routes.get('/products/:id', ProductController.show); //BUSCANDO UM PRODUTO PELO ID.
+
+//METODO DE CRIAÇÃO DE REGISTRO
+routes.post('/products', ProductController.create);
+
+//METODO DE ATUALIZAÇÃO DE REGISTRO
+routes.put('/products/:id', ProductController.update);
+
+//METODO DE EXCLUSÃO DE REGISTRO
+routes.delete('/products/:id', ProductController.delete);
 
 module.exports = routes;
